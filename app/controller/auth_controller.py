6c90ce02212a862b347b5ae0b4cd848936beeb13
TiyaAren <olesya.varyuhina@yandex.ru>
2025-08-13 00:06:34 +0300

    добавление эндпоинта /me

--- app/controller/auth_controller.py
+++ app/controller/auth_controller.py
@@ -1,13 +1,17 @@
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
 from sqlalchemy.orm import Session
 from app.configuration.database import SessionLocal, get_db
-from app.configuration.security import create_access_token
+from app.configuration.security import create_access_token, SECRET_KEY, ALGORITHM
+from app.repository.user_repository import get_user_by_email
 from app.service.user_service import register_user, login_user
 from app.schema.user_schema import UserCreate, UserLogin, UserOut, LoginResponse
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 db: Session = Depends(get_db)
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 
 @router.post("/register", response_model=UserOut)
@@ -30,3 +34,18 @@ def login(user: UserLogin, db: Session = Depends(get_db)):
         id_user=db_user.id,
         name=db_user.name
     )
+
+@router.get("/me", response_model=UserOut)
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_email: str = payload.get("sub")
+        if user_email is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    user = get_user_by_email(db, user_email)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
--- app/repository/user_repository.py
+++ app/repository/user_repository.py
@@ -1,3 +1,5 @@
+import uuid
+
 from sqlalchemy.orm import Session
 from app.model.user_model import User
 from app.schema.user_schema import UserCreate
@@ -8,6 +10,7 @@ def get_user_by_email(db: Session, email: str):
 
 def create_user(db: Session, user: UserCreate):
     db_user = User(
+        id=str(uuid.uuid4()),  # генерируем UUID в строке
         name=user.name,
         email=user.email,
         hashed_password=hash_password(user.password)
